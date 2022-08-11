900bb3fc2fcc89fe9c62030760abdc08ae6234ef
Celus8 <celus217@gmail.com>
2022-08-11 14:33:36 +0200

    Implement depth-first methods, add possibility of unbalanced trees

--- code.rb
+++ code.rb
@@ -1,6 +1,6 @@
 class Node
   include Comparable
-  attr_reader :data, :left, :right
+  attr_accessor :data, :left, :right
 
   def initialize(data, left = nil, right = nil)
     @data = data
@@ -35,9 +35,24 @@ class Tree
     @root = Node.new(array[mid], left_tree, right_tree)
   end
 
-  def insert(value)
-    @array.insert(1, value).sort
-    build_tree(@array)
+  def insert(value, node = @root)
+    return if @array.include?(value)
+
+    if value < node.data
+      if node.left.nil? || node.left.data.nil?
+        node.left = Node.new(value)
+        @array.push(value)
+      else
+        insert(value, node.left)
+      end
+    else
+      if node.right.nil? || node.right.data.nil?
+        node.right = Node.new(value)
+        @array.push(value)
+      else
+        insert(value, node.right)
+      end
+    end
   end
 
   def delete(value)
@@ -57,6 +72,7 @@ class Tree
       next if node.left.nil? && node.right.nil?
 
       queue.push(node.left, node.right)
+      queue.compact!
     end
   end
 
@@ -74,6 +90,63 @@ class Tree
     return values unless block_given?
   end
 
+  def inorder(node = @root, values = [], &block)
+    return node if node.nil?
+
+    inorder(node.left, values, &block)
+    block_given? ? block.call(node) : values.push(node.data)
+    inorder(node.right, values, &block)
+    return values unless block_given?
+  end
+
+  def preorder(node = @root, values = [], &block)
+    return node if node.nil?
+
+    block_given? ? block.call(node) : values.push(node.data)
+    preorder(node.left, values, &block)
+    preorder(node.right, values, &block)
+    return values unless block_given?
+  end
+
+  def postorder(node = @root, values = [], &block)
+    return node if node.nil?
+
+    postorder(node.left, values, &block)
+    postorder(node.right, values, &block)
+    block_given? ? block.call(node) : values.push(node.data)
+    return values unless block_given?
+  end
+
+  def height(node, counter = 0)
+    if node.left.nil? && node.right.nil?
+      counter
+    elsif node.left.nil?
+      height(node.right, counter + 1)
+    elsif node.right.nil?
+      height(node.left, counter + 1)
+    else
+      height(node.left, counter + 1) >= height(node.right, counter + 1) ? height(node.left, counter + 1) : height(node.right, counter + 1)
+    end
+  end
+
+  def depth(node)
+    height(@root) - height(node)
+  end
+
+  def balanced?
+    @array.each do |value|
+      next if find(value).left.nil? && find(value).right.nil?
+      unless (height(find(value).left || find(value).right) - height(find(value).right || find(value).left)).between?(-1, 1)
+        return false
+      end
+    end
+    true
+  end
+
+  def rebalance
+    build_tree(@array)
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -82,10 +155,27 @@ class Tree
 end
 
 bst = Tree.new
-bst.build_tree([1, 2, 3, 4, 5, 6, 7])
-bst.insert(8)
-bst.insert(10)
-bst.delete(3)
-bst.delete(5)
+bst.build_tree((Array.new(15) { rand(1..100) }))
+puts bst.balanced?
+print "Level order: #{bst.level_order}\n"
+print "Inorder: #{bst.inorder}\n"
+print "Preorder: #{bst.preorder}\n"
+print "Postorder: #{bst.postorder}\n"
+bst.insert(1)
+bst.insert(101)
+bst.insert(102)
+bst.insert(103)
+bst.insert(104)
+bst.insert(105)
+bst.insert(106)
+bst.insert(107)
+bst.insert(108)
 bst.pretty_print
-bst.level_order { |node| puts "Data: #{node.data}" }
+puts bst.balanced?
+bst.rebalance
+bst.pretty_print
+print "Level order: #{bst.level_order}\n"
+print "Inorder: #{bst.inorder}\n"
+print "Preorder: #{bst.preorder}\n"
+print "Postorder: #{bst.postorder}\n"
+puts bst.balanced?
